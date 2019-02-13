9d034c1f4a27ec6da8dca6a0dd239509783a30e9
YinAoXiong <yinaoxiong@gmail.com>
2019-02-13 16:46:25 +0800

    完成物料登记迁移，并使用严格模式

--- js/api/material.js
+++ js/api/material.js
@@ -1,6 +1,6 @@
 define(['api/apiobj', 'config/global'], function (api, g) {
 
-     api.material = {
+    api.material = {
         // 获取所有物料
         getAllMaterial: function () {
             return g.post_query(
@@ -26,6 +26,25 @@ define(['api/apiobj', 'config/global'], function (api, g) {
                     "clazz": clazz
                 }
             )
+        },
+        //派出物料接口
+        decrMaterialNum: function (num, clazz, sid, sname) {
+            return g.post_query('/material/decrMaterialNum', {
+                num: num,
+                clazz: clazz,
+                sid: sid,
+                sname: sname
+            });
+        },
+        //获取申请记录(注意区别申购记录,这个是学生的申请记录)
+        getApplys: function (clazz, sid, sname, startTime, endTime) {
+            return g.post_query('/material/getApplys', {
+                clazz: clazz,
+                sid: sid,
+                sname: sname,
+                startTime: startTime,
+                endTime: endTime
+            });
         }
     }
 
