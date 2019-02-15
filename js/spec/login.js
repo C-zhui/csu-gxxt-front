2bb55a8396d7867c6ecf87ce61174a30904be0b3
YinAoXiong <yinaoxiong@gmail.com>
2019-02-15 10:40:27 +0800

    登录成功后获取基本信息使用local storage存储

--- js/spec/login.js
+++ js/spec/login.js
@@ -1,29 +1,34 @@
-define(['jquery', 'swal', 'config/global','util/md5'], function ($, swal, g) {
-  $("#login_submit").click(function () {
-    let username = $('#username').val();
-    let password = $('#password').val();
-    password = hex_md5(password);
-    console.log(username);
-    console.log(password);
-    g.post_query(
-      '/login',
-      {
-        'name': username,
-        'password': password
-      }
-    ).done(function (data) {
-      if (data.status === 0) {
-        basicInfo = data.data;
-        window.location.href = './a-index.html?'+data.data["身份"]
-      }
-      else {
-        console.log(data);
-        swal(
-          '登录失败',
-          String(data.message),
-          'error'
-        );
-      }
-    }).fail(g.net_err);
-  });
+define(['jquery', 'swal', 'config/global', 'api/apiobj', 'api/user', 'util/md5',], function ($, swal, g, api) {
+    $("#login_submit").click(function () {
+        let username = $('#username').val();
+        let password = $('#password').val();
+        password = hex_md5(password);
+        console.log(username);
+        console.log(password);
+        g.post_query(
+            '/login',
+            {
+                'name': username,
+                'password': password
+            }
+        ).done(function (data) {
+            if (data.status === 0) {
+                basicInfo = data.data;
+                window.location.href = './a-index.html?' + data.data["身份"]
+                //获取用户基本信息使用local storage存储
+                api.user.getInfo().done(function (data) {
+                    if (data.status === 0) {
+                        localStorage.setItem('user', JSON.stringify(data.data));
+                    }
+                });
+            } else {
+                console.log(data);
+                swal(
+                    '登录失败',
+                    String(data.message),
+                    'error'
+                );
+            }
+        }).fail(g.net_err);
+    });
 });
